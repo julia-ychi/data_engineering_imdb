f748eead29560582f57bf210f05160d2c7b6d2ca
julia-ychi <jiyuyuan@umich.edu>
2024-04-17 19:35:44 +0000

    Add transformers

--- mage_imdb/imdb/data_loaders/read_name_basics.py
+++ mage_imdb/imdb/data_loaders/read_name_basics.py
@@ -19,11 +19,11 @@ def load_data(*args, **kwargs):
     filename = '/home/src/name.basics.tsv'
     # selected_cols = ['tconst', 'primaryTitle', 'startYear', 'runtimeMinutes', 'genres']
     chunksize = 10 ** 6
-    selected_cols = ['nconst', 'primaryName', 'birthYear', 'knownForTitles']
+    # selected_cols = ['nconst', 'primaryName', 'birthYear', 'knownForTitles']
     list_df = []
     with pd.read_csv(filename, chunksize=chunksize, sep='\t', low_memory=False) as reader:
         for chunk in reader:
-            list_df.append(chunk[selected_cols])
+            list_df.append(chunk)
     return pd.concat(list_df)
 
 
