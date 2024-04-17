f38af6162bf01834aa88034c74dfbb9fc10974ee
julia-ychi <jiyuyuan@umich.edu>
2024-04-17 05:35:55 +0000

    Update mage exporters

--- mage_imdb/imdb/data_exporters/title_ratings_export.py
+++ mage_imdb/imdb/data_exporters/title_ratings_export.py
@@ -2,7 +2,7 @@ from mage_ai.settings.repo import get_repo_path
 from mage_ai.io.config import ConfigFileLoader
 from mage_ai.io.google_cloud_storage import GoogleCloudStorage
 from pandas import DataFrame
-from os import path
+from os import path, getenv
 
 if 'data_exporter' not in globals():
     from mage_ai.data_preparation.decorators import data_exporter
@@ -18,14 +18,15 @@ def export_data_to_google_cloud_storage(df: DataFrame, **kwargs) -> None:
     """
     config_path = path.join(get_repo_path(), 'io_config.yaml')
     config_profile = 'default'
-
-    bucket_name = 'imdb_bucket_de_zoomcamp'
+    bucket_name = getenv('GCS_BUCKET_NAME')
+    # bucket_name = 'imdb_bucket_de_zoomcamp'
     object_key = 'title.ratings.parquet'
 
     GoogleCloudStorage.with_config(ConfigFileLoader(config_path, config_profile)).export(
-        df,
-        bucket_name,
-        object_key,
+        data=df,
+        bucket_name=bucket_name,
+        object_key=object_key,
+        format='parquet'
     )
 
 
